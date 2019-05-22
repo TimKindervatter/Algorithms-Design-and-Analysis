097a865b2c31a3c8429087f08be643ad56e6cbb4
Tim <kindert1@tcnj.edu>
2019-05-21 23:02:20 -0400

    Added docstrings and modified __main__ to print answers

--- Assignment 7/greedy_scheduling.py
+++ Assignment 7/greedy_scheduling.py
@@ -1,17 +1,19 @@
 def greedy_difference(jobs):
+    """ Computes a greedy schedule based on the difference of a job's weight and its length. Ties broken by scheduling higher weighted jobs first."""
     schedule = sorted(jobs, key=lambda x: (x[0] - x[1], x[0]), reverse=True)
     return schedule
 
 
 def greedy_ratio(jobs):
+    """ Computes a greedy schedule based on the ratio of a job's weight to its length."""
     return sorted(jobs, key=lambda x: x[0]/x[1], reverse=True)
     
 
-
-def sum_weighted_completion_times(jobs):
+def sum_weighted_completion_times(schedule):
+    """ Computes the sum of the weighted completion times for the jobs in a given schedule."""
     sum = 0
     completion_time = 0
-    for job in jobs:
+    for job in schedule:
         completion_time += job[1]
         sum += job[0]*completion_time
 
@@ -23,4 +25,11 @@ if __name__ == '__main__':
         for line in f.readlines():
             data.append([int(x) for x in line.strip().split()])
 
+    jobs = data[1:] #First line is number of jobs, not needed
+
+    difference_schedule = greedy_difference(jobs)
+    ratio_schedule = greedy_ratio(jobs)
+
+    print(sum_weighted_completion_times(difference_schedule))
+    print(sum_weighted_completion_times(ratio_schedule))
     
--- Assignment 7/test_greedy_scheduling.py
+++ Assignment 7/test_greedy_scheduling.py
@@ -1,9 +1,9 @@
 import pytest
 from greedy_scheduling import *
 
-t1 = ([[1,1], [2,1], [3,1]], [[3,1], [2,1], [1,1]])
-t2 = ([[1,1], [1,2], [1,3]], [[1,1], [1,2], [1,3]])
-t3 = ([[2,1], [3,2], [5,5]], [[3,2], [2,1], [5,5]])
+t1 = ([[1,1], [2,1], [3,1]], [[3,1], [2,1], [1,1]]) #Example from lecture, each job has same length
+t2 = ([[1,1], [1,2], [1,3]], [[1,1], [1,2], [1,3]]) #Example from lecture, each job has same weight
+t3 = ([[2,1], [3,2], [5,5]], [[3,2], [2,1], [5,5]]) #Tie breaker test for greedy difference algorithm
 
 test_cases = [t1, t2, t3]
 
@@ -13,7 +13,7 @@ def test_greedy_difference(jobs, expected):
 
     assert(schedule == expected)
 
-t3 = ([[2,1], [3,2], [5,5]], [[2,1], [3,2], [5,5]])
+t3 = ([[2,1], [3,2], [5,5]], [[2,1], [3,2], [5,5]]) #Greedy ratio will schedule these jobs differently than greedy difference
 
 test_cases = [t1, t2, t3]
 
@@ -26,9 +26,9 @@ def test_greedy_ratio(jobs, expected):
 t1 = ([[1,1], [2,1], [3,1]], 10, greedy_ratio)
 t2 = ([[1,1], [1,2], [1,3]], 10, greedy_ratio)
 t3 = ([[2,1], [3,2], [5,5]], 51, greedy_ratio)
-t4 = ([[2,1], [3,2], [5,5]], 51, greedy_difference)
+t4 = ([[2,1], [3,2], [5,5]], 52, greedy_difference)
 
-test_cases = [t1, t2, t3]
+test_cases = [t1, t2, t3, t4]
 
 @pytest.mark.parametrize('jobs, expected, f', test_cases)
 def test_sum_weighted_completion_times(jobs, expected, f):
