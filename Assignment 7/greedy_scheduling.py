51771b7d58e83ce1c319d4a89482f38c7cf2c640
Tim <kindert1@tcnj.edu>
2019-05-22 21:15:09 -0400

    Abstracted representations of jobs' weights and lengths

--- Assignment 7/greedy_scheduling.py
+++ Assignment 7/greedy_scheduling.py
@@ -1,12 +1,12 @@
 def greedy_difference(jobs):
     """ Computes a greedy schedule based on the difference of a job's weight and its length. Ties broken by scheduling higher weighted jobs first."""
-    schedule = sorted(jobs, key=lambda x: (x[0] - x[1], x[0]), reverse=True)
+    schedule = sorted(jobs, key=lambda job: (weight(job) - length(job), weight(job)), reverse=True)
     return schedule
 
 
 def greedy_ratio(jobs):
     """ Computes a greedy schedule based on the ratio of a job's weight to its length."""
-    return sorted(jobs, key=lambda x: x[0]/x[1], reverse=True)
+    return sorted(jobs, key=lambda job: weight(job)/length(job), reverse=True)
     
 
 def sum_weighted_completion_times(schedule):
@@ -14,10 +14,20 @@ def sum_weighted_completion_times(schedule):
     sum = 0
     completion_time = 0
     for job in schedule:
-        completion_time += job[1]
-        sum += job[0]*completion_time
+        completion_time += length(job)
+        sum += weight(job)*completion_time
 
     return sum
+
+
+def weight(job):
+    """ Jobs are represented as two-element lists. The first element of each job list is the job's weight."""
+    return job[0]
+
+
+def length(job):
+    """ Jobs are represented as two-element lists. The second element of each job list is the job's length."""
+    return job[1]
 
 if __name__ == '__main__':
     data = []
