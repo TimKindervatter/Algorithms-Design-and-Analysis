fe581260cc7980ae22a542b91ee1e73f37289677
Tim <kindert1@tcnj.edu>
2019-06-07 23:52:22 -0400

    Added random test cases for integers larger than 4 digits long

--- Karatsuba_Multiplication/test_karatsuba.py
+++ Karatsuba_Multiplication/test_karatsuba.py
@@ -1,4 +1,5 @@
 import pytest
+from random import randint
 from karatsuba import karatsuba
 
 t0 = (34, 57, 1938)
@@ -7,7 +8,10 @@ t2 = (78, 34, 2652)
 t3 = (56+78, 12+34, 6164)
 t4 = (5678, 1234, 7006652)
 
-test_cases = [t0, t1, t2, t3, t4]
+random_tests = [(randint(0, 123456789), randint(0, 123456789)) for _ in range(20)]
+random_tests = [random_tests[i] + (random_tests[i][0]*random_tests[i][1],) for i in range(len(random_tests))]
+
+test_cases = [t0, t1, t2, t3, t4] + random_tests
 
 @pytest.mark.parametrize('x, y, expected', test_cases)
 def test_karatsuba(x, y, expected):
