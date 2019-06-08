c36d1fb3115f1531a03ac83a2faf8d4052b8ecf7
Tim <kindert1@tcnj.edu>
2019-06-08 00:02:03 -0400

    Added tests for negative numbers

--- Karatsuba_Multiplication/test_karatsuba.py
+++ Karatsuba_Multiplication/test_karatsuba.py
@@ -8,7 +8,7 @@ t2 = (78, 34, 2652)
 t3 = (56+78, 12+34, 6164)
 t4 = (5678, 1234, 7006652)
 
-random_tests = [(randint(0, 123456789), randint(0, 123456789)) for _ in range(20)]
+random_tests = [(randint(-1e64, 1e64), randint(-1e64, 1e64)) for _ in range(100)]
 random_tests = [random_tests[i] + (random_tests[i][0]*random_tests[i][1],) for i in range(len(random_tests))]
 
 test_cases = [t0, t1, t2, t3, t4] + random_tests
