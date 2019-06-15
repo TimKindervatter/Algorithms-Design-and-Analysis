59616664a543a45d131f6373a3fa1d8c594891bf
Tim <kindert1@tcnj.edu>
2019-06-15 13:48:03 -0400

    Updated UnionFind class with several new features

--- Clustering/UnionFind.py
+++ Clustering/UnionFind.py
@@ -1,9 +1,19 @@
+from networkx.utils import groups
+
+
 class UnionFind:
-    def __init__(self, parents, ranks):
-        self.parent = parents
-        self.rank = ranks
+    def __init__(self, nodes):
+        self.parent = {}
+        self.rank = {}
+        for node in nodes:
+            self.parent[node] = node
+            self.rank[node] = 0
 
     def union(self, p, q):
+        """ 
+        Combines the disjoint subset that contains p with the disjoint subset that contains q.
+        Unions are performed by rank. In case of ties, the root of p's subset remains the root of the unioned subset.
+        """
         s1 = self.find(p)
         s2 = self.find(q)
 
@@ -14,10 +24,9 @@ class UnionFind:
             self.parent[s2] = s1
         elif self.rank[s1] < self.rank[s2]:
             self.parent[s1] = s2
-        
-        return self.parent
 
     def find(self, x):
+        """ Returns the leader of the disjoint subset containing x."""
         root = self.parent[x]
 
         while self.parent[root] != root:
@@ -25,3 +34,16 @@ class UnionFind:
             root = self.parent[root]
 
         return root
+
+    def __str__(self):
+        """ The union-find is represented as a list of disjoint subsets."""
+        subsets = sorted(map(sorted, self.get_subsets()))
+        return str(subsets)
+
+    def __len__(self):
+        return len(self.get_subsets())
+
+    def get_subsets(self):
+        """ Groups elements by the subset they belong to and returns a list of all subsets."""
+        return list(groups(self.parent).values())
+
--- Clustering/test_UnionFind.py
+++ Clustering/test_UnionFind.py
@@ -3,26 +3,56 @@ from UnionFind import UnionFind
 
 
 #Union-Find is 1-indexed
-t1 = ({1:1, 2:1, 3:1, 4:4, 5:4, 6:4}, {1:1, 2:0, 3:0, 4:1, 5:0, 6:0}, 3, 1)
-t2 = ({1:1, 2:1, 3:1, 4:4, 5:4, 6:4}, {1:1, 2:0, 3:0, 4:1, 5:0, 6:0}, 4, 4)
+t1 = ([x for x in range(1,7)], 3, 3)
+t2 = ([x for x in range(1,7)], 0, 0)
 
-test_cases = [t1, t2]
+test_cases = [t1, pytest.param(*t2, marks=pytest.mark.xfail)]
 
-@pytest.mark.parametrize('parents, ranks, x, expected', test_cases)
-def test_find(parents, ranks, x, expected):
-    clusters = UnionFind(parents, ranks)
+@pytest.mark.parametrize('nodes, x, expected', test_cases)
+def test_find(nodes, x, expected):
+    clusters = UnionFind(nodes)
     root = clusters.find(x)
 
     assert(root == expected)
 
 
-t1 = ({1:1, 2:1, 3:1, 4:4, 5:4, 6:4}, {1:1, 2:0, 3:0, 4:1, 5:0, 6:0}, 1, 4, {1:1, 2:1, 3:1, 4:1, 5:4, 6:4})
+t1 = ([x for x in range(1, 7)], 1, 4, {1:1, 2:2, 3:3, 4:1, 5:5, 6:6})
+t2 = ([x for x in range(1, 7)], 4, 1, {1:4, 2:2, 3:3, 4:4, 5:5, 6:6})
+t3 = ([x for x in range(1, 7)], 1, 1, {1:1, 2:2, 3:3, 4:4, 5:5, 6:6})
+
+test_cases = [t1, t2, t3]
+
+@pytest.mark.parametrize('nodes, p, q, expected', test_cases)
+def test_union(nodes, p, q, expected):
+    clusters = UnionFind(nodes)
+    clusters.union(p, q)
+
+    new_parents = clusters.parent
+
+    assert(new_parents == expected)
+
+
+t1 = ('xyz', "[['x', 'y'], ['z']]")
 
 test_cases = [t1]
 
-@pytest.mark.parametrize('parents, ranks, p, q, expected', test_cases)
-def test_union(parents, ranks, p, q, expected):
-    clusters = UnionFind(parents, ranks)
-    new_parents = clusters.union(p, q)
+@pytest.mark.parametrize('nodes, expected', test_cases)
+def test_print(nodes, expected):
+    clusters = UnionFind(nodes)
+    clusters.union('x', 'y')
+    string = clusters.__str__()
 
-    assert(new_parents == expected)
+    assert(string == expected)
+
+
+t1 = ('xyz', 2)
+
+test_cases = [t1]
+
+@pytest.mark.parametrize('nodes, expected', test_cases)
+def test_len(nodes, expected):
+    clusters = UnionFind(nodes)
+    clusters.union('x', 'y')
+    length = clusters.__len__()
+
+    assert(length == expected)
