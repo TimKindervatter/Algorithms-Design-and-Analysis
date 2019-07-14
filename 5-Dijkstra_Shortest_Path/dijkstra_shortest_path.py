7df6133ba80c07e55908412119dffddbe58dca6a
Tim <kindert1@tcnj.edu>
2019-07-13 21:28:48 -0400

    Moved Dijkstra test cases to their own file

--- 5-Dijkstra_Shortest_Path/dijkstra_shortest_path.py
+++ 5-Dijkstra_Shortest_Path/dijkstra_shortest_path.py
@@ -11,7 +11,12 @@ def dijkstra(adj_list, source_node):
     """
     V = list(adj_list.keys())
     X = [source_node]
-    A = [0] * (len(V) + 1)
+
+    # Given graph is 1-indexed but python is 0-indexed. V = {1, 2, ..., n}, so len(V) = n. 
+    # Zero is not included in V, so we must initialize n + 1 = len(V) + 1 total elements.
+    # Index zero is never used, and the elements of interest are indexed 1 through n inclusive.
+    # This is done so that the resultant vertices are indexed the same way as in the problem statement.
+    A = [0] * (len(V) + 1) 
     
     while not (set(X) == set(V)):
         greedy_score = float('inf')
@@ -28,26 +33,18 @@ def dijkstra(adj_list, source_node):
     return A
         
 
-test_case = False
-if test_case:
-    #Adjacency list of example graph from lectures
-    adj_list = {
-            1: {2:1, 3:4},
-            2: {3:2, 4:6},
-            3: {4:3},
-            4: {}
-            }
-else:
-    #Reads in adjacency list from text file and parses it into a dictionary
+if __name__ == '__main__':
+    # Reads in adjacency list from text file and parses it into a dictionary
     temp_list = []
     file = open('dijkstraData.txt')
     for line in file:
         temp_list.append(line.split())
         
-        adj_list = dict((int(row[0]), dict((int(el.split(',')[0]), int(el.split(',')[1])) for el in row[1:])) for row in temp_list)
+    adj_list = dict((int(row[0]), dict((int(el.split(',')[0]), int(el.split(',')[1])) for el in row[1:])) for row in temp_list)
 
-#Run Dijkstra's algorithm on the graph represented by adj_list, starting from node 1
-A = dijkstra(adj_list, 1)
+    # Run Dijkstra's algorithm on the graph represented by adj_list, starting from node 1 (since input graph is 1-indexed)
+    A = dijkstra(adj_list, 1)
 
-#The shortest paths to the nodes requested in the problem statement
-answers = [A[7], A[37], A[59], A[82], A[99], A[115], A[133], A[165], A[188], A[197]]
+    # The shortest paths to the nodes requested in the problem statement
+    answers = [A[7], A[37], A[59], A[82], A[99], A[115], A[133], A[165], A[188], A[197]]
+    print(answers)
